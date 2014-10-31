5899c3668dd28d94bca1486a8aa1c77403a06e5d
amlozano1 <lockes5hadow@gmail.com>
2014-10-30 21:18:23 -0700

    Added try catch
    Added try catch on tool-belt specific imports.

--- AtomCCD/AtomCCD/settings.py
+++ AtomCCD/AtomCCD/settings.py
@@ -15,10 +15,13 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config()
-}
+try:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
+except:
+    pass
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
