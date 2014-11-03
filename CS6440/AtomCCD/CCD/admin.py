253fa7018fb8c5ce514fe6f63066e3c754fc075e
Jon Brahser <jon.brasher@gatech.edu>
2014-11-03 02:19:46 -0500

    removing CS6440 directory

--- CS6440/AtomCCD/CCD/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from CCD.models import Atom
-# Register your models here.
-
-
-admin.site.register(Atom)
--- CS6440/AtomCCD/CCD/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'anthony.lozano'
-from django.conf.urls import patterns, url
-
-from CCD import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-)
