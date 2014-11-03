bffdbd0940cf70a86b2df2be85980e6cd8e4ae2c
Jon Brahser <jon.brasher@gatech.edu>
2014-11-03 17:59:06 -0500

    working on statics and adding bootstrap

--- AtomCCD/AtomCCD/settings.py
+++ AtomCCD/AtomCCD/settings.py
@@ -29,14 +29,21 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static', 'static_root'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'static_dirs'),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+MEDIA_URL = '/media/'
 
 try:
     from local_settings import *
