d1a5dc3f57f75d6d445f30a24fe33043144d71ce
amlozano1 <lockes5hadow@gmail.com>
2014-11-26 00:52:35 -0700

    Snapshot view
    Added a 'snapshot' view of the patient to the top.  Its a greatly
    simplified demographics designed for quick consumption. The idea is that
    the Physician can then open the demographics accordian if they need more
    info

--- AtomCCD/CCD/templatetags/ccd_extras.py
+++ AtomCCD/CCD/templatetags/ccd_extras.py
@@ -13,5 +13,26 @@ def dob_to_age(string_date):
     :param string_date: a string like 00/00/0000
     :return: Current age in years
     """
-    date = datetime.strptime(string_date, "%d/%m/%Y")
+    try:
+        date = datetime.strptime(string_date, "%d/%m/%Y")
+    except Exception:
+        try:
+            date = datetime.strptime(string_date, "%m/%d/%Y") # I guess date order can be american OR internatl. Great.
+        except Exception:
+            return 0 # Worst case just say we don't know. #TODO add dateutils package to handle dates. Who the hell wrote the CCD standard and decided NOT to use ISO datestamps???
     return int((datetime.now() - date).days / 365.25)
+
+@register.filter
+@stringfilter
+def lang(lang_code):
+    """
+    It seems the CCDs we have for testing are not using ISO 639-1 or any RFC I can find. Bogus. For now we are just
+    going to handle them on a case by case basis.
+    :param lang_code:
+    :return:
+    """
+    if lang_code == "eng":
+        return "English (US)"
+    elif lang_code == "fr-CN":  # CN? really? Thats China, not canada. They don't speak french in china.
+        return "French (CA)"
+    return "None"
